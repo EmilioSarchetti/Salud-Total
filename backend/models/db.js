a3f6c2ef0a25e6bfd45d22f315d68618ce17ef08
EmilioSarchetti <emiliosarchetti@gmail.com>
2025-11-06 00:05:15 -0300

    aplicacion de .env

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,15 +1,22 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+require("dotenv").config();
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'salud_total'
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 10,
 });
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Conectado a la base de datos MySQL');
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error al conectar con MySQL:", err);
+  } else {
+    console.log("Conectado a la base de datos MySQL");
+    connection.release();
+  }
 });
 
-module.exports = connection;
+module.exports = db;
