eb220b4b4deec0885a7d67f93f3e90153f5ffb7b
EmilioSarchetti <emiliosarchetti@gmail.com>
2025-12-09 00:20:42 -0300

    correccion del push erroneo

--- backend/routes/turnosRoutes.js
+++ backend/routes/turnosRoutes.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const turnosController = require('../controllers/turnosControllers');
-
-router.get('/medicoes/ocupados/:medicoId/:fecha', turnosController.obtenerHorariosOcupados);
-
-router.get('/', turnosController.listarTodosLosTurnos);
-
-module.exports = router;
-=======
 const express = require('express');
 const router = express.Router();
 const turnosController = require('../controllers/turnosController');
@@ -18,4 +7,3 @@ router.get('/medicos/ocupados/:medicoId/:fecha', turnosController.obtenerHorario
 router.get('/', turnosController.listarTodosLosTurnos);
 
 module.exports = router;
->>>>>>> 316f8a7ba45d30582304a1b1663ebff3e3f72e9d
