c610be5958e699cb8ee7b07f8b81f9868a8a8382
EmilioSarchetti <emiliosarchetti@gmail.com>
2025-12-09 00:19:34 -0300

    correccion de archivo css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-<<<<<<< HEAD
-//import './index.css'
-import App from './App.jsx'
-=======
 import App from "./App.jsx";
 import './styles.css';
->>>>>>> 316f8a7ba45d30582304a1b1663ebff3e3f72e9d
 
 
 createRoot(document.getElementById("root")).render(
